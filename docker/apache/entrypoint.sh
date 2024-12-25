65bf825d1b7c94f7c128284f4f95479b89f434d5
Safouane Affane <safouane200403@hotmail.com>
2024-12-25 22:07:41 +0100

    Pour conversion scripts

--- docker/apache/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-# Installer les dépendances Composer si elles n'existent pas
-echo "Installation des dépendances Composer..."
-composer install --no-scripts --no-interaction --optimize-autoloader
-
-# Démarrer Apache
-exec apache2-foreground "$@"
